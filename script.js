ea6e35e7ba3b29113d018e81b81b29954b541a96
Berresu <90646787+Berresu@users.noreply.github.com>
2022-09-26 16:46:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,96 @@
-function hesapla(){
-    let dogumYili=document.getElementById("dogumYili").value;
-    let baslananYil=document.getElementById("baslananYil").value;
-    let hesaplanacakYil=document.getElementById("hesaplanacakYil").value;
-    let sonuc=document.getElementById("sonuc");
-    let yas=hesaplanacakYil-dogumYili;
-    let calismaYili=hesaplanacakYil-baslananYil;
-    console.log(hesaplanacakYil);
-    console.log(baslananYil);
-    console.log(dogumYili);
-    console.log(calismaYili);
+document.getElementById("emekli").addEventListener("change", hesapla);
+document.getElementById("emekliDegil").addEventListener("change", hesapla);
+let i;
 
-    if(yas<=50 && calismaYili>=15){
-        sonuc.innerHTML="İzin Gününüz 26 Gün";
+function sec(){
+    let tarih1=new Date();
+    let yil=tarih1.getFullYear();
+
+    for(let i=1; i<=31; i++){
+        let dogumGunu=document.createElement("option");
+        document.getElementById("numberDogumGunu").appendChild(dogumGunu);
+        dogumGunu.innerHTML=i;
+        dogumGunu.value=i;
     }
-    else if(yas>18 && calismaYili>=6){
-        sonuc.innerHTML="İzin Gününüz 20 Gün";
+    for(let i=1; i<=12; i++){
+        let dogumAyi=document.createElement("option");
+        document.getElementById("numberDogumAyi").appendChild(dogumAyi);
+        dogumAyi.innerHTML=i;
+        dogumAyi.value=i;
     }
-    else if(yas>18 && calismaYili<=5){
-        sonuc.innerHTML="İzin Gününüz 14 Gün";
+    for(let i=yil; i>=1950; i--){
+        let dogumYili=document.createElement("option");
+        document.getElementById("numberDogumYili").appendChild(dogumYili);
+        dogumYili.innerHTML=i;
+        dogumYili.value=i;
     }
-    else if(yas<=18){
-        sonuc.innerHTML="İzin Gününüz 20 Gün";
+
+    for(let i=1; i<=31; i++){
+        let baslamaGunu=document.createElement("option");
+        document.getElementById("numberBaslamaGunu").appendChild(baslamaGunu);
+        baslamaGunu.innerHTML=i;
+        baslamaGunu.value=i;
+    }
+    for(let i=1; i<=12; i++){
+        let baslamaAyi=document.createElement("option");
+        document.getElementById("numberBaslamaAyi").appendChild(baslamaAyi);
+        baslamaAyi.innerHTML=i;
+        baslamaAyi.value=i;
+    }
+    for(let i=yil; i>=1950; i--){
+        let baslamaYili=document.createElement("option");
+        document.getElementById("numberBaslamaYili").appendChild(baslamaYili);
+        baslamaYili.innerHTML=i;
+        baslamaYili.value=i;
+    }
+
+    for(let i=1; i<=31; i++){
+        let hesaplanacakGun=document.createElement("option");
+        document.getElementById("numberHesaplanacakGun").appendChild(hesaplanacakGun);
+        hesaplanacakGun.innerHTML=i;
+        hesaplanacakGun.value=i;
+    }
+    for(let i=1; i<=12; i++){
+        let hesaplanacakAy=document.createElement("option");
+        document.getElementById("numberHesaplanacakAy").appendChild(hesaplanacakAy);
+        hesaplanacakAy.innerHTML=i;
+        hesaplanacakAy.value=i;
+    }
+    for(let i=yil; i>=1950; i--){
+        let hesaplanacakYil=document.createElement("option");
+        document.getElementById("numberHesaplanacakYil").appendChild(hesaplanacakYil);
+        hesaplanacakYil.innerHTML=i;
+        hesaplanacakYil.value=i;
     }
 }
 
-function emekliHesapla(){
-    document.getElementById("sonuc").innerHTML="İzin Gününüz 20 Gün";
+function hesapla(){
+    let dogulanYil=document.getElementById("numberDogumYili").value;
+    let baslananYil=document.getElementById("numberBaslamaYili").value;
+    let hesaplananYil=document.getElementById("numberHesaplanacakYil").value;
+
+    if(document.getElementById("emekliDegil").checked){
+        if((hesaplananYil-dogulanYil)<=18){
+            document.getElementById("sonuc").innerHTML="İzin Gününüz "+(hesaplananYil-baslananYil)*20+" Gün";
+        }
+        else if((hesaplananYil-dogulanYil)>18 && (hesaplananYil-baslananYil)<=5){
+            document.getElementById("sonuc").innerHTML="İzin Gününüz "+(hesaplananYil-baslananYil)*14+" Gün";
+        }
+        else if((hesaplananYil-dogulanYil)>18 && (hesaplananYil-baslananYil)>5){
+            document.getElementById("sonuc").innerHTML="İzin Gününüz "+(hesaplananYil-baslananYil)*20+" Gün";
+        }
+        else if((hesaplananYil-dogulanYil)>18 && (hesaplananYil-baslananYil)>=15){
+            document.getElementById("sonuc").innerHTML="İzin Gününüz "+(hesaplananYil-baslananYil)*26+" Gün";
+        }
+    }
+    else{
+        document.getElementById("emekliDegil").style.display="none";
+    }
+
+    if(document.getElementById("emekli").checked){
+        document.getElementById("sonuc").innerHTML="İzin Gününüz 20 Gün";
+    }
+    else{
+        document.getElementById("emekli").style.display="none";
+    }
 }
